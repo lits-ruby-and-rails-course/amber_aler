bfe4a95aa9217ab768a7be078d9128c96501b464
KushnerukAndriy <kushneruk.andriy@gmail.com>
2016-11-29 17:52:37 +0200

    CRUD works

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,5 +1,25 @@
 class EmergenciesController < ApplicationController
+	before_action :find_emergency, only: [ :show, :destroy]
+
 	def index
 		@emergencies = Emergency.all
 	end
+	
+	def show
+	end
+	
+	def create
+
+	end
+	
+	def destroy
+
+	end
+
+	private
+
+	def find_emergency
+		@emergency = Emergency.find(params[:id])
+	end
+
 end
