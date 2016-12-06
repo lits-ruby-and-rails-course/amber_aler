a18867b6ee8d66786b2eb5ab6fd3a9a0eadc1be6
Taras Kunch <taras.kunch@gmail.com>
2016-12-06 03:54:34 +0200

    Implement avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,21 @@ module ApplicationHelper
     res = truncate_html(res, length: shorten, omission: '...') if shorten.present?
     res
   end
+
+  def avatar_url(user, size: 200, real: false)
+    gravatar_id = email_hexdigest_for user
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}#{real && "&d=404"}"
+  end
+
+  def avatar_tag(user, size: 200, alt: nil, **options)
+    image_tag(
+      avatar_url(user, size: options[:size], real: options[:real]),
+      alt: (options[:alt] || user.name),
+      class: options[:class],
+      data: {hash: email_hexdigest_for(user)})
+  end
+
+  def email_hexdigest_for(user)
+    Digest::MD5.hexdigest(user.email&.downcase)
+  end
 end
