72875afa8553bd9bd0774296d630f89c5ac9edc8
Taras Kunch <taras.kunch@gmail.com>
2016-12-06 21:19:37 +0200

    Claim_closed functionality implementation

--- app/models/message.rb
+++ app/models/message.rb
@@ -20,6 +20,10 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :emergency
 
+  before_save do |record|
+    emergency.update(status: Emergency::CLOSED) if claim_closed?
+  end
+
   before_save    :touch_emergency!
   before_destroy :touch_emergency!
 
