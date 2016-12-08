3cf47898b9d880577c74d6f7e2229339aa339ad2
Taras Kunch <taras.kunch@gmail.com>
2016-12-08 20:26:52 +0200

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
+  around_filter :set_time_zone
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def set_locale
+    I18n.locale = params[:locale]
+    I18n.locale ||= current_user&.try(:locale)
+    I18n.locale ||= extract_locale_from_accept_language_header
+    I18n.locale ||= I18n.default_locale
+  end
+
+  def set_time_zone(&block)
+    time_zone = params[:time_zone] || current_user.try(:time_zone) || 'Kyiv'
+    Time.use_zone(time_zone, &block)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  # def authenticate_user!(*args)
-  #   p args
-  #   if user_signed_in?
-  #     super
-  #   else
-  #     redirect_to new_user_session_path, :notice => 'please sign in'
-  #   end
-  # end
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
